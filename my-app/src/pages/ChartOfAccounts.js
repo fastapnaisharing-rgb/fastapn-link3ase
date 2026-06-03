5c1bda744adf8555edd31ce0502a092657425da0
Karn <fastapnaisharing@gmail.com>
2026-06-03 16:56:26 +0700

    update

--- my-app/src/pages/ChartOfAccounts.js
+++ my-app/src/pages/ChartOfAccounts.js
@@ -646,7 +646,7 @@ function ChartOfAccounts({ activeSubTab, onSubTabChange, flyoutOpen = false }) {
             </thead>
           </table>
         </div>
-        <div ref={tbodyRef} style={S.tbodyWrap} onScroll={syncScroll}>
+        <div ref={tbodyRef} style={S.tbodyWrap} className="table-scroll" onScroll={syncScroll}>
           <table style={{ ...S.table, width: `${totalW}px` }}>
             {renderColGroup(COLUMNS_SCALED)}
             <tbody>
